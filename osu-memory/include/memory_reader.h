f4052a3c843c9cf391688846a7d0fc53b9db5075
UnnamedOrange <lycheng1215@sina.com>
2021-02-18 16:16:33 +0800

    Implement opening the process in memory_reader.

--- osu-memory/include/memory_reader.h
+++ osu-memory/include/memory_reader.h
@@ -3,3 +3,73 @@
 
 #pragma once
 
+#include <string>
+#include <stdexcept>
+#include <thread>
+
+#include <Windows.h>
+#undef min
+#undef max
+
+namespace osu_memory
+{
+	/// <summary>
+	/// Read memory from a process.
+	/// Select process by id first, and then you can read the memory.
+	/// </summary>
+	class memory_reader
+	{
+	private:
+		HANDLE hProcess{};
+		HANDLE hEvent_exit{ CreateEventW(nullptr, TRUE, FALSE, nullptr) };
+
+	private:
+		std::thread exit_thread;
+		void exit_thread_proc();
+
+	public:
+		memory_reader() noexcept = default;
+		memory_reader(DWORD dwProcessId) : memory_reader()
+		{
+			select_process(dwProcessId);
+		}
+		memory_reader(const memory_reader&) = delete;
+		memory_reader(memory_reader&&) = delete;
+		memory_reader& operator=(const memory_reader&) = delete;
+		memory_reader& operator=(memory_reader&&) = delete;
+		virtual ~memory_reader()
+		{
+			detach();
+			CloseHandle(hEvent_exit);
+		}
+
+	public:
+		/// <summary>
+		/// Returns whether it is NOT avaliable to read the process memory.
+		/// Note that the function returning false doesn't mean reading memory always succeeds.
+		/// It may still fail because of the process status.
+		/// Namely, the process may have ended while empty() is false.
+		/// <remarks>See try_detach.</remarks>
+		/// </summary>
+		bool empty() const;
+		/// <summary>
+		/// Detach only if the process has been terminated.
+		/// If the process hasn't been terminated, the operation won't be done.
+		/// It's recommanded that you call this function before any memory reading.
+		/// </summary>
+		/// <returns>Whether the detach operation is done.</returns>
+		bool try_detach();
+		/// <summary>
+		/// Select a process by its PID to read memory.
+		/// If you have already selected a process, the previous one will be abandoned.
+		/// If it fails to open the process (PID is invalid, or access denied, etc.),
+		/// an std::runtime_error will be thrown.
+		/// </summary>
+		void select_process(DWORD dwProcessId);
+		/// <summary>
+		/// Forcedly detach. This function will be automatically called when this memory_reader is begin destructed.
+		/// </summary>
+		/// <returns>Whether the detach is done. Equals to !empty().</returns>
+		bool detach();
+	};
+}
--- osu-memory/src/memory_reader.cpp
+++ osu-memory/src/memory_reader.cpp
@@ -3,3 +3,58 @@
 
 #include "memory_reader.h"
 
+void osu_memory::memory_reader::exit_thread_proc()
+{
+	HANDLE waits[]{ hEvent_exit, hProcess };
+	WaitForMultipleObjects(static_cast<DWORD>(std::size(waits)),
+		waits, FALSE, INFINITE);
+	ResetEvent(hEvent_exit);
+	CloseHandle(hProcess);
+	hProcess = nullptr;
+}
+
+bool osu_memory::memory_reader::empty() const
+{
+	return !exit_thread.joinable();
+}
+bool osu_memory::memory_reader::try_detach()
+{
+	if (empty())
+		return false;
+	bool ret = false;
+	if (!hProcess)
+	{
+		detach();
+		ret = true;
+	}
+	return ret;
+}
+void osu_memory::memory_reader::select_process(DWORD dwProcessId)
+{
+	detach();
+
+	{
+		HANDLE hProcess_temp = OpenProcess(SYNCHRONIZE | PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, dwProcessId);
+		DWORD dwError = GetLastError();
+		// It is possible that the process has been terminated but OpenProcess succeeded.
+		if (!hProcess_temp || WaitForSingleObject(hProcess_temp, 1) == WAIT_OBJECT_0)
+		{
+			if (hProcess_temp)
+				CloseHandle(hProcess_temp);
+			throw std::runtime_error("Fail to OpenProcess. The error code is " + std::to_string(dwError) + ".");
+		}
+		hProcess = hProcess_temp;
+		ResetEvent(hEvent_exit);
+	}
+	exit_thread = std::move(std::thread(&memory_reader::exit_thread_proc, this));
+}
+bool osu_memory::memory_reader::detach()
+{
+	if (empty())
+		return false;
+
+	SetEvent(hEvent_exit);
+	exit_thread.join();
+
+	return true;
+}
