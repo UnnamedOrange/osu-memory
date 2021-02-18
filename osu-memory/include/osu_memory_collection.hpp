1150a9f2c25caa017b579347562806d4529663d7
UnnamedOrange <lycheng1215@sina.com>
2021-02-18 21:17:05 +0800

    Add osu_memory_collection.

--- osu-memory/include/osu_memory_collection.hpp
+++ osu-memory/include/osu_memory_collection.hpp
@@ -1,12 +1,16 @@
 ﻿// Copyright (c) UnnamedOrange. Licensed under the MIT Licence.
 // See the LICENSE file in the repository root for full licence text.
 
-// This header includes all other headers in this library.
-
 #pragma once
 
-#include "memory_reader.h"
-#include "osu_memory_reader.h"
 #include "osu_memory_module_base.hpp"
 #include "osu_memory_module_mode.hpp"
-#include "osu_memory_collection.hpp"
+
+namespace osu_memory
+{
+	class osu_memory_collection :
+		public osu_memory_module_mode
+	{
+
+	};
+}
