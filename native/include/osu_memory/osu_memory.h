c3a0618a1ce421386e9f29c9a7aabdfa2254308a
UnnamedOrange <lycheng1215@sina.com>
2021-07-15 21:21:37 +0800

    Reset.

--- native/include/osu_memory/osu_memory.h
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) UnnamedOrange. Licensed under the MIT Licence.
-// See the LICENSE file in the repository root for full licence text.
-
-#pragma once
-
-#include <osu_memory/process.h>
-#include <osu_memory/imp_base.h>
-#include <osu_memory/imp_hits.h>
-#include <osu_memory/reader.h>
