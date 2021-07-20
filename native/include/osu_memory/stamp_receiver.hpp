f6fb3e2e29acc4a1ec5bbb088a574e98fde34a12
UnnamedOrange <lycheng1215@sina.com>
2021-07-20 17:16:48 +0800

    Implement stamp_receiver.

---
+++ native/include/osu_memory/stamp_receiver.hpp
@@ -0,0 +1,47 @@
+﻿// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+#pragma once
+
+#include <type_traits>
+
+#include <osu_memory/stamp_recorder.hpp>
+
+namespace osu_memory::utils
+{
+	/// <summary>
+	/// Stamp receiver.
+	/// </summary>
+	template <bool is_atomic_stamp>
+	class enable_stamp_receiver
+	{
+	public:
+		using stamp_base_t = typename enable_stamp_recorder<is_atomic_stamp>::stamp_base_t;
+		using stamp_t = typename enable_stamp_recorder<is_atomic_stamp>::stamp_t;
+
+	public:
+		/// <summary>
+		/// Reduce C5204.
+		/// </summary>
+		virtual ~enable_stamp_receiver() = default;
+
+	private:
+		stamp_t crt{};
+	protected:
+		/// <summary>
+		/// Returns the stamp from the source.
+		/// </summary>
+		virtual stamp_base_t get_stamp() = 0;
+	public:
+		/// <summary>
+		/// Update the current stamp to the latest.
+		/// </summary>
+		/// <returns>If the stamp is different from the source, returns true. Otherwise false.</returns>
+		bool update()
+		{
+			bool ret = crt != get_stamp();
+			crt = get_stamp();
+			return ret;
+		}
+	};
+}
---
+++ native/include/osu_memory/stamp_receiver_impl.hpp
@@ -0,0 +1,23 @@
+﻿// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+#pragma once
+
+#include <type_traits>
+
+#include <osu_memory/osu_daemon.hpp>
+#include <osu_memory/stamp_receiver.hpp>
+
+namespace osu_memory::utils
+{
+	/// <summary>
+	/// Stamp receiver. Implenmented with osu_daemon.
+	/// </summary>
+	class enable_stamp_receiver_impl : public osu_memory::utils::enable_stamp_receiver<false>
+	{
+		virtual stamp_base_t get_stamp() override
+		{
+			return osu_memory::os::osu_daemon::get_instance().get_stamp();
+		}
+	};
+}
