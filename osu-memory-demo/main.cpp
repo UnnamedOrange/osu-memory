1c1b7feeabc78131fe463c635a03b905b820eae8
UnnamedOrange <lycheng1215@sina.com>
2021-07-21 23:29:42 +0800

    Update demo.

--- osu-memory-demo/main.cpp
+++ osu-memory-demo/main.cpp
@@ -8,46 +8,51 @@
 
 int main(int argn, char** argv)
 {
+	UNREFERENCED_PARAMETER(argn);
+	UNREFERENCED_PARAMETER(argv);
+
 	using namespace osu_memory;
 	using namespace std::chrono;
 	using namespace std::literals;
+
+	reader r;
 
 	while (true)
 	{
 		std::system("cls");
 
 		{
-			auto t = reader::get_miss(false);
+			auto t = r.get_hit_miss();
 			if (t)
 				std::cout << "miss: " << *t << std::endl;
 		}
 		{
-			auto t = reader::get_50(false);
+			auto t = r.get_hit_50();
 			if (t)
 				std::cout << "50: " << *t << std::endl;
 		}
 		{
-			auto t = reader::get_100(false);
+			auto t = r.get_hit_100();
 			if (t)
 				std::cout << "100: " << *t << std::endl;
 		}
 		{
-			auto t = reader::get_200(false);
+			auto t = r.get_hit_200();
 			if (t)
 				std::cout << "200: " << *t << std::endl;
 		}
 		{
-			auto t = reader::get_300(false);
+			auto t = r.get_hit_300();
 			if (t)
 				std::cout << "300: " << *t << std::endl;
 		}
 		{
-			auto t = reader::get_perfect(false);
+			auto t = r.get_hit_perfect();
 			if (t)
 				std::cout << "perfect: " << *t << std::endl;
 		}
 		{
-			auto t = reader::get_keys(false);
+			auto t = r.get_mania_keys();
 			if (t)
 			{
 				const auto& a = *t;
