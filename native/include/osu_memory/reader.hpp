9a98e4b165d3102e7b7401d8d7642089565b65ca
UnnamedOrange <lycheng1215@sina.com>
2021-07-21 22:54:36 +0800

    Implement reader.

---
+++ native/include/osu_memory/reader.hpp
@@ -0,0 +1,18 @@
+﻿// Copyright (c) UnnamedOrange. Licensed under the MIT License.
+// See the LICENSE file in the repository root for full license text.
+
+#pragma once
+
+#include <osu_memory/reader_hit.hpp>
+#include <osu_memory/reader_mania_keys.hpp>
+
+namespace osu_memory
+{
+	/// <summary>
+	/// Memory reader collection.
+	/// </summary>
+	class reader :
+		public reader_hit,
+		public reader_mania_keys
+	{};
+}
